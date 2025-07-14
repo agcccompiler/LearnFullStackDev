3ff5cbb0f524013f9f0a9e93f4492f6972402910
Jincheng Guo <jincheng.guo.gz@gmail.com>
2025-07-14 14:16:39 +0800

    for fun

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 #!/opt/anaconda3/bin/python3
 # -*- coding: utf-8 -*-
-import sys 
+import os
 
-with open('test.txt', 'a') as f:
-    f.write('1')
+current_path = os.path.abspath('.')
+sub_dir = "JustForFun"
+file_name = "Fun.py"
+
+dir_path = os.path.join(current_path, sub_dir)
+full_path = os.path.join(current_path, sub_dir, file_name)
+
+if os.path.exists(full_path):
+    pass
+else: 
+    os.mkdir(full_path)
